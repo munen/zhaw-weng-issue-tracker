3f536c76b9a378da0bc89783639b10413995fcd8
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2016-03-02 08:09:12 +0100

    testing cljs tests on gitlab ci

--- test/cljs/issue_tracker/core_test.cljs
+++ test/cljs/issue_tracker/core_test.cljs
@@ -1,8 +1,36 @@
 (ns issue-tracker.core-test
   (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]
-            [reagent.core :as reagent :refer [atom]]
-            [issue-tracker.core :as rc]))
+            [reagent.core :as r :refer [atom]]))
 
-(deftest test-home
-  (is (= true true)))
+(defn setup []
+  (def issues-counter (r/atom 0))
 
+  (def new-issue (r/atom {:priority 1
+                          :date (-> (js/Date.) .toISOString (.slice 0 10))}))
+
+  (def issues-atom (r/atom []))
+
+
+  (defn add-issue-to-list []
+    (swap! issues-atom conj {:key @issues-counter
+                             :name (:name @new-issue)
+                             :priority (:priority @new-issue)
+                             :date (:date @new-issue)})
+    (swap! issues-counter inc))
+
+  (swap! new-issue assoc :priority 1)
+  (swap! new-issue assoc :date "2016-12-12")
+  (swap! new-issue assoc :name "foo")
+  (swap! new-issue assoc :completed false)
+  )
+
+
+(deftest toggle-issue
+  (testing "Should toggle a specific issue"
+    (setup)
+    (is (= (:completed (first (issue-tracker.core/toggle-issue 0)))
+           true))
+    (is (= (:completed (first (issue-tracker.core/toggle-issue 0)))
+           false))
+    (is (= (:completed (first (issue-tracker.core/toggle-issue 0)))
+           true))))
